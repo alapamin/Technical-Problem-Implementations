5fdc73adca36a7b413e995f746e00959a9eda53f
Cadaur <alapamin@gmail.com>
2022-07-28 12:46:48 -0400

    More elegant solution

--- Graphs/Redundant_Connection.py
+++ Graphs/Redundant_Connection.py
@@ -1,9 +1,13 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        #Run union find
-        #If when finding union if rank[p1] == 2 or rank[p2] == 2 -> set answer to edge
-        par = [i for i in range(0,len(edges))]
-        rank = [1 for i in range(0,len(edges))]
+        #Union Find
+        #When parents equal one another when adding a new edge, this is one of the redundant connections
+        #Keep saving that to the ans ans you iterate through the edges
+        
+        #Faster than BFS
+        
+        par = [i for i in range(len(edges))]
+        rank = [1] * len(edges)
         
         def find(n1):
             res = n1
@@ -15,26 +19,23 @@ class Solution:
         def union(n1,n2):
             p1,p2 = find(n1),find(n2)
             
-            if p1==p2:
-                return [n1,n2] #Any time we create a union between two nodes who already have the same parent -> this is the 3rd added edge.
+            if p1 == p2:
+                return [n1,n2]
             if rank[p2] > rank[p1]:
                 par[p1] = p2
-                rank[p2]+=rank[p1]
+                rank[p2] += rank[p1]
             else:
                 par[p2] = p1
-                rank[p1]+=rank[p2]
+                rank[p1] += rank[p2]
             return []
-        
-        ans = []
-        for i in range(len(edges)):
-            for j in range(len(edges[i])):
-                edges[i][j]-=1
-                
+        res = []
         for n1,n2 in edges:
-            current = union(n1,n2)
-            if len(current) == 2:
-                ans = current
-        ans[0]+=1
-        ans[1]+=1
-        return ans
+            cN1 = n1 - 1
+            cN2 = n2 - 1
+            currentAns = union(cN1,cN2)
+            if currentAns:
+                res = currentAns
                 
+        return [res[0]+1,res[1]+1]
+            
+            
